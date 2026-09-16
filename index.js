a5c2754ab6b44a234a82d71c46375fd128d16ab9
Vera Mensah <veramensah2004@gmail.com>
2026-09-16 21:50:38 +0000

    Add POST /items endpoint

--- index.js
+++ index.js
@@ -2,10 +2,24 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello from the backend!');
+app.use(express.json());
+
+let items = [
+  { itemName: "Apple", quantity: 10, price: 100 },
+  { itemName: "Milk", quantity: 5, price: 80 },
+];
+
+app.get('/items', (req, res) => {
+  res.json(items);
+});
+
+app.post('/items', (req, res) => {
+    const newItems = req.body;
+    items.push(newItems);
+    res.json(newItems);
 });
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
+
